74d65ed8e1ede38e5c9fd8a0fa7c4dc26960dcfe
drestrep <davidro112014@gmail.com>
2026-05-19 13:16:35 +0200

    CPP08 updated ex02 main

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include "MutantStack.hpp"
 #include "MutantStack.tpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
@@ -9,10 +10,12 @@ int main()
 	mstack.push(5);
 	mstack.push(17);
 	std::cout << "Top element: " << mstack.top() << std::endl;
-	mstack.pop();
 	std::cout << "Size: " << mstack.size() << std::endl;
+	mstack.pop();
 
-	std::cout << "\n --------------------- \n\n";
+	std::cout << std::endl;
+	std::cout << "Top element: " << mstack.top() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
@@ -20,13 +23,42 @@ int main()
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+
 	while (it != ite)
 	{
 		std::cout << "Element: " << *it << std::endl;
 		++it;
 	}
+
+	std::cout << "\n --------------------- \n\n";
+
 	std::stack<int> s(mstack);
+	while (s.size() > 0)
+	{
+		std::cout << "Stack element: " << s.top() << std::endl;
+		s.pop();
+	}
+	std::cout << "\n --------------------- \n\n";
+
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << "Top element: " << lst.back() << std::endl;
+	lst.pop_back();
+	std::cout << "Size: " << lst.size() << std::endl;
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+	std::list<int>::iterator it_lst = lst.begin();
+	std::list<int>::iterator ite_lst = lst.end();
+	++it_lst;
+	--it_lst;
+	while (it_lst != ite_lst){
+		std::cout << "Element: " << *it_lst << std::endl;
+		++it_lst;
+	}
+
 	return 0;
 }
