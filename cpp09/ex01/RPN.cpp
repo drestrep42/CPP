c3839efbab7504ca8385f7697cdc0874a44db94e
David <drestrep@student.42madrid.com>
2026-06-03 13:59:58 +0200

    CPP09 ex01 started

---
+++ cpp09/ex01/RPN.cpp
@@ -0,0 +1,14 @@
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::~RPN() {}
+
+RPN::RPN(const RPN& other) : _stack(other._stack) {}
+
+RPN& RPN::operator=(const RPN& other) {
+    if (this != &other) {
+        _stack = other._stack;
+    }
+    return *this;
+}
---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,24 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+class RPN {
+private:
+    std::stack<int> _stack;
+
+public:
+    RPN();
+    ~RPN();
+    RPN(const RPN& other);
+    RPN& operator=(const RPN& other);
+
+    void evaluate(const std::string& expression);
+    void push(int value);
+    int pop();
+    int top();
+};
+
+#endif
